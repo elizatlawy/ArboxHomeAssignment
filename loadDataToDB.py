3b0714d89c3b0c953967174ef92380fe7dfc2afe
Eli Zat <eli.zatlawy@gmail.com>
2021-08-07 13:54:00 +0300

    minor refactoring

--- loadDataToDB.py
+++ loadDataToDB.py
@@ -12,17 +12,6 @@ def generateUniqueID():
     return str(uuid4())
 
 
-# assume that we have all the necessary headers but their name may change in different exel sheets
-def setHeaders(dataFrameHeaders):
-    dataHeaders.FIRST_NAME = dataFrameHeaders[0]
-    dataHeaders.LAST_NAME = dataFrameHeaders[1]
-    dataHeaders.EMAIL = dataFrameHeaders[2]
-    dataHeaders.PHONE = dataFrameHeaders[3]
-    dataHeaders.MEMBERSHIP_START_DATE = dataFrameHeaders[4]
-    dataHeaders.MEMBERSHIP_END_DATE = dataFrameHeaders[5]
-    dataHeaders.MEMBERSHIP_NAME = dataFrameHeaders[6]
-
-
 def parseArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument("dataPath", help="Path to the Data Excel Sheet to insert into the Database")
@@ -38,28 +27,37 @@ def readExelSheet(path, sheet=0):
         print("Error while opening Excel Sheet file: " + str(e))
 
 
-def getDuplicateEmailsFromData(emails):
-    dup = emails[emails.duplicated(keep=False)].tolist()
-    return set(dup)
-
-
-def processData(args):
-    dataFrame = readExelSheet(args.dataPath)
-    dataFrameHeaders = dataFrame.columns.values
+# check if the exel file have the necessary headers in order to input the data to the DB
+def checkHeaders(dataFrameHeaders):
     if len(dataFrameHeaders) < 5:
         sys.exit("Exel Sheet is missing one of the necessary headers: first_name, last_name, phone, email,"
                  "membership_start_date membership_end_date or membership_name")
-    setHeaders(dataFrameHeaders)
-    emails = dataFrame[dataHeaders.EMAIL]
-    duplicateEmailsFromData = getDuplicateEmailsFromData(emails)
-    if duplicateEmailsFromData:
-        sys.exit("The data file contains the following duplicate emails: \n" + str(duplicateEmailsFromData) +
-                 "\nPlease delete the duplication and try ""again")
+
+
+# assume that we have all the necessary headers but their name may change in different exel sheets
+def setHeaders(dataFrameHeaders):
+    dataHeaders.FIRST_NAME = dataFrameHeaders[0]
+    dataHeaders.LAST_NAME = dataFrameHeaders[1]
+    dataHeaders.EMAIL = dataFrameHeaders[2]
+    dataHeaders.PHONE = dataFrameHeaders[3]
+    dataHeaders.MEMBERSHIP_START_DATE = dataFrameHeaders[4]
+    dataHeaders.MEMBERSHIP_END_DATE = dataFrameHeaders[5]
+    dataHeaders.MEMBERSHIP_NAME = dataFrameHeaders[6]
+
+
+def checkDuplicateEmailsInData(emails):
+    dupEmails = set(emails[emails.duplicated(keep=False)].tolist())
+    if dupEmails:
+        sys.exit("Duplicate emails are not allowed.\nThe exel sheet contains the following duplicate emails: \n" + str(dupEmails) +
+                 "\nPlease delete the duplications and try ""again")
+
+
+def checkDuplicateEmailsInDB(emails):
     duplicateEmailsFromDB = pr.getDuplicateEmailsFromDB(emails)
     if duplicateEmailsFromDB > 0:
-        sys.exit("The DB already contains the following emails from the exel sheet:" + str(duplicateEmailsFromDB)
-                 + " please delete the duplication and try again")
-    insertDataToDB(dataFrame, args.clubId)
+        sys.exit("Duplicate emails are not allowed. \n The DB already contains the following emails from the exel "
+                 "sheet:" + str(duplicateEmailsFromDB)
+                 + " please delete the duplications and try again")
 
 
 def insertDataToDB(dataFrame, clubId):
@@ -87,6 +85,18 @@ def insertMembershipToDB(row, usersId):
                                        row[dataHeaders.MEMBERSHIP_END_DATE].strftime("%d/%m/%y"),
                                        row[dataHeaders.MEMBERSHIP_NAME])
     pr.repo.membership.insert(membershipToInsert)
+
+
+# main function that handle the data process
+def processData(args):
+    dataFrame = readExelSheet(args.dataPath)
+    dataFrameHeaders = dataFrame.columns.values
+    checkHeaders(dataFrameHeaders)
+    setHeaders(dataFrameHeaders)
+    emails = dataFrame[dataHeaders.EMAIL]
+    checkDuplicateEmailsInData(emails)
+    checkDuplicateEmailsInDB(emails)
+    insertDataToDB(dataFrame, args.clubId)
 
 
 if __name__ == '__main__':
